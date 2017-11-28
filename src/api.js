0359d88df20a86271f7fab54555fca7f7c6d90c4
mducle <minhduc95@lozi.vn>
2017-11-28 16:56:07 +0700

    use local images

--- src/api.js
+++ src/api.js
@@ -1,14 +1,135 @@
-import axios from "axios";
-import language from "./language.json";
+import axios from 'axios';
+import language from './language.json';
 
-const base = "https://pro.musehall.com/";
+const base = 'https://pro.musehall.com/';
 export function fetchGalleryData(unknownId1, unknownId2) {
-  const url = `${base}private/visite/${unknownId1}/${unknownId2}`;
-  return axios
-    .get(url)
-    .then(res => res.data)
-    .then(
-      ({
+  return Promise.resolve({
+    imgSalle1: [
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      }
+    ],
+    imgSalle2: [
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      }
+    ],
+    imgSalle3: [
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      }
+    ],
+    imgSalle4: [
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      },
+      {
+        image:
+          'http://www.doanhnhansaigon.vn/ic/l/o/w620h405f1c1-files-articles-2015-1093936-lozi-doanhnhansaigon.jpg'
+      }
+    ],
+    nbRoom: 4,
+    name: 'a',
+    title: 'a',
+    subtitle: 'b'
+  }).then(
+    ({
+      id,
+      company,
+      date,
+      description,
+      start,
+      end,
+      free,
+      imgSalle1,
+      imgSalle2,
+      imgSalle3,
+      imgSalle4,
+      nbRoom,
+      mainImage,
+      name,
+      title,
+      subtitle,
+      theme,
+      price,
+      profileImg
+    }) => {
+      const imageRooms = [imgSalle1, imgSalle2, imgSalle3, imgSalle4].filter(
+        room => room.length > 0
+      );
+      const numOfImagesPerWall = Math.min(
+        ...imageRooms.map(room => room.length),
+        3
+      );
+      let preloadImagePromise = [];
+
+      const rooms = imageRooms.map(room =>
+        room.reduce((room, image, imageIndex) => {
+          const wallIndex = Math.floor(imageIndex / numOfImagesPerWall);
+          if (!room[wallIndex]) room[wallIndex] = [];
+          // image.image = language.urlImage + image.image;
+          preloadImagePromise.push(
+            new Promise(resolve => {
+              let img = new Image();
+              img.onload = resolve;
+              img.src = image.image;
+            })
+          );
+          room[wallIndex].push(image);
+          return room;
+        }, [])
+      );
+
+      Promise.all(preloadImagePromise);
+      return {
         id,
         company,
         date,
@@ -16,10 +137,7 @@ export function fetchGalleryData(unknownId1, unknownId2) {
         start,
         end,
         free,
-        imgSalle1,
-        imgSalle2,
-        imgSalle3,
-        imgSalle4,
+        rooms,
         nbRoom,
         mainImage,
         name,
@@ -28,52 +146,7 @@ export function fetchGalleryData(unknownId1, unknownId2) {
         theme,
         price,
         profileImg
-      }) => {
-        const imageRooms = [imgSalle1, imgSalle2, imgSalle3, imgSalle4].filter(
-          room => room.length > 0
-        );
-        const numOfImagesPerWall = Math.min(
-          ...imageRooms.map(room => room.length),
-          3
-        );
-        let preloadImagePromise = [];
-
-        const rooms = imageRooms.map(room =>
-          room.reduce((room, image, imageIndex) => {
-            const wallIndex = Math.floor(imageIndex / numOfImagesPerWall);
-            if (!room[wallIndex]) room[wallIndex] = [];
-            image.image = language.urlImage + image.image;
-            preloadImagePromise.push(
-              new Promise(resolve => {
-                let img = new Image();
-                img.onload = resolve;
-                img.src = image.image;
-              })
-            );
-            room[wallIndex].push(image);
-            return room;
-          }, [])
-        );
-
-        Promise.all(preloadImagePromise);
-        return {
-          id,
-          company,
-          date,
-          description,
-          start,
-          end,
-          free,
-          rooms,
-          nbRoom,
-          mainImage,
-          name,
-          title,
-          subtitle,
-          theme,
-          price,
-          profileImg
-        };
-      }
-    );
+      };
+    }
+  );
 }
--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import "./global.css";
-import { fetchGalleryData } from "./api";
-import Gallery from "./components/gallery";
+import './global.css';
+import { fetchGalleryData } from './api';
+import Gallery from './components/gallery';
 
 class App extends Component {
   state = { loading: true, data: undefined };
   componentDidMount() {
-    fetchGalleryData(
-      "349464684",
-      "52f3b663c746ae2ca5ffd95ba85d748e"
-    ).then(data => this.setState({ loading: false, data }));
+    fetchGalleryData('349464684', '52f3b663c746ae2ca5ffd95ba85d748e').then(
+      data => this.setState({ loading: false, data })
+    );
   }
 
   render() {
     const { loading, data } = this.state;
     if (loading) return <div>Loading</div>;
+    console.log(data);
     return <Gallery {...data} />;
   }
 }
